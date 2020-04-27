c54572896f9a46752a486223635d92ff2ac06913
Oscar Villanueva <javiondo68@gmail.com>
2020-04-26 20:17:41 -0500

    limit to 3 in best clients

--- db/resolvers.js
+++ db/resolvers.js
@@ -169,7 +169,7 @@ const resolvers = {
                     $sort: { total: -1}
                 },
                 {
-                    $limit: 10
+                    $limit: 3
                 }
             ])
 
